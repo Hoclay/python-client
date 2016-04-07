cd67bd62610bd1418a7fcee8495c65a35461a992
Ford Hurley <ford.hurley@gmail.com>
2016-04-07 14:38:13 -0400

    Run tests against python2 and python3 using tox
    Currently failing on 3.

--- README.rst
+++ README.rst
@@ -3,3 +3,21 @@ Handwriting.io Python Client
 
 .. image:: http://handwriting.io/images/hwio_logo_black.png
         :target: https://handwriting.io
+
+Running tests
+-------------
+
+.. code-block:: bash
+
+    $ pip install -r requirements.txt
+    $ pip install -r requirements-dev.txt
+    $ py.test
+
+To run the tests against multiple versions of python:
+
+.. code-block:: bash
+
+    $ python setup.py test
+
+You will need to have each of the tested versions of python installed on your
+system.
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 with open('README.rst', 'r') as f:
   readme = f.read()
 
 def strip_requirement(line):
-  if not line or line.startswith('-') or line.startswith('#'):
+  # skip blank lines or comments
+  if not line or line.startswith('#'):
     return
   return line.strip()
 
@@ -14,6 +16,30 @@ with open('requirements.txt', 'r') as f:
 with open('requirements-dev.txt', 'r') as f:
   test_requirements = [strip_requirement(x) for x in f if strip_requirement(x)]
 
+
+class Tox(TestCommand):
+  user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+  def initialize_options(self):
+    TestCommand.initialize_options(self)
+    self.tox_args = None
+
+  def finalize_options(self):
+    TestCommand.finalize_options(self)
+    self.test_args = []
+    self.test_suite = True
+
+  def run_tests(self):
+    #import here, cause outside the eggs aren't loaded
+    import tox
+    import shlex
+    args = self.tox_args
+    if args:
+        args = shlex.split(self.tox_args)
+    errno = tox.cmdline(args=args)
+    sys.exit(errno)
+
+
 setup(
   name='handwriting',
   version='0.0.1',
@@ -21,6 +47,6 @@ setup(
   long_description=readme,
   packages=['handwriting'],
   install_requires=requirements,
-  setup_requires=['pytest-runner'],
-  tests_require=test_requirements,
+  tests_require=['tox'],
+  cmdclass = {'test': Tox},
 )
