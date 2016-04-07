bb1d8a9e041be72eb17913bbba87cc92dbb5be9c
Ford Hurley <ford.hurley@gmail.com>
2016-04-07 16:42:32 -0400

    Render tests

--- test/test_client.py
+++ test/test_client.py
@@ -9,3 +9,21 @@ def test_list_handwritings(client):
 def test_get_handwriting(client, test_handwriting):
   hw = client.get_handwriting(test_handwriting['id'])
   assert hw == test_handwriting
+
+
+def test_render_png(client, test_handwriting):
+  image = client.render_png({
+      'handwriting_id': test_handwriting['id'],
+      'text': 'Hello world!',
+  })
+  assert image
+  assert isinstance(image, bytes)
+
+
+def test_render_pdf(client, test_handwriting):
+  image = client.render_pdf({
+      'handwriting_id': test_handwriting['id'],
+      'text': 'Hello world!',
+  })
+  assert image
+  assert isinstance(image, bytes)
